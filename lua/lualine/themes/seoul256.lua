045b2be30d7be371ffddda18aad2c2af402cf126
Marian Werny <marian@werny.eu>
2024-01-22 13:42:49 +0100

    updated lualine

--- lua/lualine/themes/seoul256.lua
+++ lua/lualine/themes/seoul256.lua
@@ -4,34 +4,37 @@ local M = {}
 
 M.normal = {
     a = { fg = colors.c237, bg = colors.c173, gui = "bold" },
-    b = { fg = colors.c95, bg = colors.c236 },
-    c = { fg = colors.c252, bg = colors.selection },
+    b = { fg = colors.c252, bg = colors.c236 },
+    c = { fg = colors.c95, bg = colors.c187},
 }
 
 M.insert = {
 	a = { fg = colors.c237, bg = colors.c108, gui = "bold" },
-	b = { fg = colors.c95, bg = colors.c236 },
+	b = { fg = colors.c252, bg = colors.c236 },
 }
 
 M.visual = {
 	a = { fg = colors.c237, bg = colors.purple, gui = "bold" },
-	b = { fg = colors.c95, bg = colors.c236 },
+	b = { fg = colors.c252, bg = colors.c236 },
 }
 
 M.replace = {
 	a = { fg = colors.c237, bg = colors.c174, gui = "bold" },
-	b = { fg = colors.c95, bg = colors.c236 },
+	b = { fg = colors.c252, bg = colors.c236 },
 }
 
 M.command = {
-    a = { fg = colors.c237, bg = colors.c179, gui = "bold" },
-    b = { fg = colors.c95, bg = colors.c236 },
+	a = { fg = colors.c237, bg = colors.c179, gui = "bold" },
+	b = { fg = colors.c252, bg = colors.c236 },
 }
 
 M.inactive = {
-    a = { fg = colors.c240, bg = colors.c237, gui = "bold" },
-    b = { fg = colors.c240, bg = colors.c237 },
-    c = { fg = colors.c240, bg = colors.c23 },
+	a = { fg = colors.c240, bg = colors.c237, gui = "bold" },
+	b = { fg = colors.c240, bg = colors.c237 },
+	c = { fg = colors.c240, bg = colors.c23 },
 }
 
+--red: c95
+--yellow: c187
+-- good fg grey: c252
 return M
